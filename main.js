9a07f3cae841a310faeb4b826af1e2bed9dc483b
Aleksej Eremenko <aleksej.eremenko579@gmail.com>
2024-07-30 21:33:09 +0300

    fix:(cube remove)

--- main.js
+++ main.js
@@ -1,23 +1,13 @@
-import './style.css'
-// import { setupCounter } from './counter.js'
+document.addEventListener('DOMContentLoaded', () => {
+    // let allD = document.querySelectorAll('main article');
+    // allD.forEach((item) => {
+    //     let degValue = 0;
+    //     item.addEventListener('click', (e) => {
+    //         if (!e.target.closest('details')) {
+    //             item.querySelector('.cube__wrapper').style.transform = `rotateY(${degValue + 90}deg)`;
+    //             degValue += 90;
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
-
-
-// setupCounter(document.querySelector('#counter'))
+    //         }
+    //     });
+    // });
+});
