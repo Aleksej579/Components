f5dbe1c4066ab3246d39375517a0dbf90ac217d1
Aleksej579 <aleksej.eremenko579@gmail.com>
2025-10-02 12:12:42 +0300

    fix:()

--- main.js
+++ main.js
@@ -1,13 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // let allD = document.querySelectorAll('main article');
-    // allD.forEach((item) => {
-    //     let degValue = 0;
-    //     item.addEventListener('click', (e) => {
-    //         if (!e.target.closest('details')) {
-    //             item.querySelector('.cube__wrapper').style.transform = `rotateY(${degValue + 90}deg)`;
-    //             degValue += 90;
-
-    //         }
-    //     });
-    // });
-});
+// document.addEventListener('DOMContentLoaded', () => {});
